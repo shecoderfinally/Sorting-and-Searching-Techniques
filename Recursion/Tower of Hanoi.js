59ac0722fe6977e729d570c5723082de89a5a8d5
fakeshashank21 <91732167+fakeshashank21@users.noreply.github.com>
2021-10-01 16:36:55 +0530

    Create Tower of Hanoi.js

---
+++ Recursion/Tower of Hanoi.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function stepsToSolveHanoiT(height, srcP, desP, bufferP) {
+  if (height >= 1) {
+
+    // Move a tower of height-1 to the buffer peg, using the destination peg.
+    stepsToSolveHanoiT(height - 1, srcP, bufferP, desP);
+
+    // Move the remaining disk to the destination peg.
+    console.log('Move disk from Tower ', srcP, ' to Tower ', desP);
+
+    // Move the tower of `height-1` from the `buffer peg` to the `destination peg` using the `source peg`.        
+    stepsToSolveHanoiT(height - 1, bufferP, desP, srcP);
+  }
+  
+  return;
+}
+
+stepsToSolveHanoiT(3, "A", "C", "B");
+
+// Move disk from Tower  A  to Tower  C
+// Move disk from Tower  A  to Tower  B
+// Move disk from Tower  C  to Tower  B
+// Move disk from Tower  A  to Tower  C
+// Move disk from Tower  B  to Tower  A
+// Move disk from Tower  B  to Tower  C
+// Move disk from Tower  A  to Tower  C
