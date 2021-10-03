38ef3e1316776ce50adb635822fd5c761e552a22
Atul Singh <59884618+AtulSingh-Emyre@users.noreply.github.com>
2021-10-03 10:10:55 +0530

    Created D_Deleting Divisors.java
    problem link: https://codeforces.com/contest/1537/problem/D

---
+++ Codeforces_Problem/D_Deleting Divisors.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+           br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+           while (st == null || !st.hasMoreElements()) {
+              try {
+                 st = new StringTokenizer(br.readLine());
+              }
+              catch (IOException e) {
+                 e.printStackTrace();
+              }
+           }
+           return st.nextToken();
+        }
+        int nextInt() {
+           return Integer.parseInt(next());
+        }
+        long nextLong() {
+           return Long.parseLong(next());
+        }
+        double nextDouble() {
+           return Double.parseDouble(next());
+        }
+        String nextLine() {
+           String str = "";
+           try {
+              str = br.readLine();
+           }
+           catch (IOException e) {
+              e.printStackTrace();
+           }
+           return str;
+        }
+     }
+     static void ln(String s){
+        System.out.println(s);   
+     }
+     
+     static int abs(int a,int b){
+      if(b>a) return b-a;
+      return a-b;
+  }
+  
+  public static char nextChar(char curr) {
+     if(curr == 'z') return 'a';
+     else return (char)((int)curr+1);
+  }
+  
+  public static int factorial(int i) {
+     int res = 1;
+     while(i>0) res*=i--;
+     return res;
+  }
+
+   static HashSet<Long> cache = new HashSet<>();
+  public static void main (String[] args) throws java.lang.Exception
+  {
+   FastReader fs = new FastReader();     
+   int t = fs.nextInt();
+   while(t-->0) {
+      int n = fs.nextInt();
+      String A = "Alice";
+      String B = "Bob";
+      if (n%2==1 || n==2 || n==8 || n==32 ||n==128 ||n==512 || n==2048 ||n==8192||n==32768||n==131072||n==524288|| n==2097152||n==8388608||n==33554432||n==134217728||n==536870912)
+			System.out.println(B);
+		else
+		   System.out.println(A);
+   }
+  }
+}
