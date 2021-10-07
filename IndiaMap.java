9f1f07670967f02a42746449cd42092a4923040c
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2021-10-07 20:11:42 +0530

    IndiaMap.java
    please accept my PR

---
+++ IndiaMap.java
@@ -0,0 +1,358 @@
+public class IndiaMap
+ public static void main(String[] args)
+ {
+  for(int i=1; i<=2; i++)
+  {
+    for(int j=1; j<=14; j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=4*i+2; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<(12+i+i)+2; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=(2*8-i); j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  } 
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=14+i; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=3*(4-i+2); j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=(12+i+i)+2; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=12; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=18; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=2*i+6; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=2*(9-i+1)-1; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=2*i+12; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=2*(7-i+1); j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=3*i+13; j++)
+    {
+	System.out.print("*");		
+    }
+    for(int j=1; j<=3*(11-i+1)+1; j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=5*i; j++)
+    {
+	System.out.print("*");
+    }	
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<2*(10-5*(i)+(i*2)-1)+i-1; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int k=1; k<=7*i+16; k++)
+    {
+	System.out.print("*");
+    }
+    for(int j=1; j<=2*(8-i+1); j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=i; j++)
+    {
+	System.out.print("*");
+    }
+    for(int j=1; j<=4*(2-i+1)+1; j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=2*i+8; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }  
+  for(int i=1; i<=2; i++)
+  {
+    for(int j=1; j<=i+4; j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=24*i+13; j++)
+    {
+	System.out.print("*");
+    }
+    for(int j=i; j<=1; j++)
+    {
+	System.out.print("        **      ********");
+    }
+    System.out.println();
+  }
+  for(int i=1; i<=2; i++)
+  {
+    for(int j=1; j<=i+6; j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=2*(22-i+2); j++)
+    {
+	System.out.print("*");
+    }
+    for(int j=1; j<=i; j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=12; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=1-i+1; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=4+2*i; j++)
+    {
+	System.out.print("*");
+    }
+    for(int j=1; j<=1-i+1; j++)
+    {
+	System.out.print(" ");
+    }
+    for (int j=1; j<=(40+i+i)+2; j++)
+    {
+	System.out.print("*");
+    }
+    for (int j=1; j<=2*(4-i+1); j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=7-i; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=2*(2-i+1); j++)
+    {
+	System.out.print (" ");
+    }
+    for (int j=1; j<=4*i+45; j++)
+    {
+	System.out.print("*");
+    }
+    for (int j=1; j<=8; j++)
+    {
+	System.out.print(" ");
+    }
+    for (int j=1; j<=2*(2-i+1)-1; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=3+i; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=7-i; j++)
+    {
+	System.out.print("*");
+    }
+    for (int j=1; j<=1; j++)
+    {
+	System.out.print (" ");
+    }
+    for (int j=1; j<=40-2*i+1; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=12-i+1; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=35; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=12; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=30; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=12; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=2*(14-i+1); j++)
+    {
+	System.out.print("*");
+    }
+    for(int j=i; j<=1; j++)
+    {
+	System.out.print("                For More visit");
+    }
+    System.out.println ();
+  } 
+  for(int i=1;i<=2;i++)
+  {
+    for(int j=1;j<=i+11;j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=25-i+1; j++)
+    {
+	System.out.print("*");
+    }
+    for(int j=i; j<=1; j++)
+    {
+	System.out.print("                www.coronaupdatez.com");
+    }
+	
+    System.out.println();
+  }
+  for(int i=1;i<=2;i++)
+  {
+    for(int j=1;j<=14;j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=20-i+1; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println();
+  }
+  for(int i=1;i<=2;i++)
+  {
+    for(int j=1;j<=i+14;j++)
+    {
+	System.out.print(" ");
+    }
+    for(int j=1; j<=16; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println();
+  }  
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=i+15; j++)
+    {
+	System.out.print (" ");
+    }
+    for(int j=1; j<=15-i+1; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 3; i++)
+  {
+    for (int j=1; j<=18; j++)
+    {	
+	System.out.print (" ");
+    }
+    for(int j=1; j<=12; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 2; i++)
+  {
+    for (int j=1; j<=20; j++)
+    {
+	System.out.print (" ");
+    }
+    for (int j=1; j<=10-2*i; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+  for (int i = 1; i <= 1; i++)
+  {
+    for (int j=1; j<=21; j++)
+    {
+	System.out.print (" ");
+    }
+    for (int j=1; j<=4; j++)
+    {
+	System.out.print("*");
+    }
+    System.out.println ();
+  }
+ }
+}
