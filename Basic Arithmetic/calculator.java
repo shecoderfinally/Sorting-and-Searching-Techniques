86bd52aff27bb326defc65aa6021848335295888
Dushyantha Thilakarathne <dushyanthaat@gmail.com>
2021-10-20 13:08:32 +0530

    calculator.java
    I have updated this code with the switch-case. It will increase the readability of code and will increase the efficiency of the code.

--- Basic Arithmetic/calculator.java
+++ Basic Arithmetic/calculator.java
@@ -3,15 +3,15 @@ public class calculator {
     public static void main(String[] args) throws Exception {
 
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("----------WELCOME TO CALCULATOR-----------");
 
         System.out.println("Enter two numbers A and B");
         System.out.print("A: ");
-        int a=sc.nextInt();
+        int a = sc.nextInt();
         System.out.print("B: ");
-        int b=sc.nextInt();
+        int b = sc.nextInt();
 
         System.out.println("Choose Arithemetic Operation");
         System.out.println("Enter  * to multiplication");
@@ -19,32 +19,24 @@ public class calculator {
         System.out.println("Enter  - for subtraction");
         System.out.println("Enter  / for division ");
 
-        String ch=sc.next();
-        if(ch.equals("*")){
-
-            System.out.println(a*b);
+        char ch = sc.next().charAt(0);
+        switch (ch) {
+            case '+' -> {
+                System.out.println(a + " + " + b + " = " + (a + b));
+            }
+            case '-' -> {
+                System.out.println(a + " - " + b + " = " + (a - b));
+            }
+            case '/' -> {
+                System.out.println(a + " / " + b + " = " + (a / b));
+            }
+            case '*' -> {
+                System.out.println(a + " * " + b + " = " + (a * b));
+            }
+            default -> {
+                System.out.println("invalid selection");
+            }
         }
-        else if(ch.equals("+")){
-
-            System.out.println(a+b);
-
-        }
-        else if(ch.equals("-")){
-
-            System.out.println(a-b);
-        }
-        else if(ch.equals("/")){
-            System.out.println((double)a/(double)b);
-        }
-        else{
-            System.out.println("invalid selection");
-        }
-
-
-
-
-
-
     }
 }
 
